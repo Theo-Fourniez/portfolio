29d648cd289fcc2bd8b8f84c357073567ed382f0
Fourniez Theo <theo_fourniez@ens.univ-artois.fr>
2020-12-25 18:29:34 +0100

    refactor\!: utilisation d'une librairie pour les boites modales

--- js/modals.js
+++ js/modals.js
@@ -1,39 +1,7 @@
-const modalClassName = "mymodal";
-const modalOpenName = "open";
-
-const siblings = (elem) => {
-    let siblings = [];
-
-    if (!elem.parentNode) {
-        return siblings;
-    }
-
-    let sibling = elem.parentNode.firstElementChild;
-
-    do {
-        if (sibling != elem) {
-            siblings.push(sibling);
-        }
-    } while (sibling = sibling.nextElementSibling);
-		
-    return siblings;
-};
-
-function toggleModal(){
-    var source = event.target || event.srcElement;
-    var modal = null;
-
-    if(source.classList.contains("mymodal_button")){
-        modal = source.parentElement;
-    }
-    else{
-        modal = source.nextElementSibling;
-    }
-
-    modal.classList.toggle(modalOpenName);
-
-    var body = document.getElementsByTagName("body").item(0);
-
-    body.classList.toggle("no-scroll");
-}   
-
+$('a.open-modal').click(function(event) {
+    $(this).modal({
+      fadeDuration: 250,
+      closeClass: 'icon-remove'
+    });
+    return false;
+  });
